9b5ad62adb7157b8b4d73983fd89c746b98c68d2
AtesiT <atesit@mail.ru>
2025-11-16 16:27:37 +0300

    second view controller displays sections with rows

--- ContactList/ViewControllers/SecondContactsListViewController.swift
+++ ContactList/ViewControllers/SecondContactsListViewController.swift
@@ -9,16 +9,35 @@ final class SecondContactsListViewController: UITableViewController {
     }
 }
 
+// MARK: - Settings of table
 extension SecondContactsListViewController {
+    // The count of rows in one section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    // The count of sections in our the table
+    override func numberOfSections(in tableView: UITableView) -> Int {
         contactList.count
     }
+    
+    // Set a title for sections
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "\(contactList[section].name) \(contactList[section].surname)"
+    }
+
+    // Settings of a cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "phoneAndMailCell", for: indexPath)
-        let contact = contactList[indexPath.row]
+        let contact = contactList[indexPath.section]
         var content = cell.defaultContentConfiguration()
         
-        content.text = contact.name
+        if indexPath.row == 0 {
+            content.text = "📞 \(contact.number)"
+        } else {
+            content.text = "✉️ \(contact.email)"
+        }
+        
         cell.contentConfiguration = content
         return cell
     }
